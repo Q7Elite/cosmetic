7bb92e7164fea09865d82c00af1b9f10a53914fe
Q7Elite <karichuflora2016@gmail.com>
2024-10-22 14:22:56 +0300

    Implement arrowUp icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+// import { ArrowUpIcon } from "@heroicons/react/solid";
+import { ArrowUpIcon } from "@heroicons/react/24/solid";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
@@ -15,12 +17,21 @@ import Footer from "./components/Footer/Footer";
 import Popup from "./components/Popup/Popup";
 import MapSection from "./components/Map/MapSection";
 import Gallery from "./components/gallery/Gallery";
+
 const App = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
 
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -30,6 +41,7 @@ const App = () => {
     });
     AOS.refresh();
   }, []);
+
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <NavBar handleOrderPopup={handleOrderPopup} />
@@ -46,6 +58,14 @@ const App = () => {
       <Testimonials />
       <Footer />
       <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+
+      {/* Back to Top Button with Icon */}
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-4 right-4 p-2 bg-purple-500 text-white rounded-full shadow-lg"
+      >
+        <ArrowUpIcon className="h-6 w-6" />
+      </button>
     </div>
   );
 };
